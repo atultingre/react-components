7925915599d5d7257cd0e6f579cd38e33032d623
ATUL TINGRE <atultingre.work@gmail.com>
2023-04-24 17:41:11 +0530

    Skeleton Component Added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 // import './App.css';
 import InfiniteScroll from './InfiniteScroll/InfiniteScroll';
+import SkeletonPost from './Skeleton/SkeletonPost';
 import SearchAndFilter from './searchAndFilter/SearchAndFilter';
 
 function App() {
   return (
     <div className="App">
+      <SkeletonPost/>
+      <br />
+      <hr />
+      <br />
       <SearchAndFilter/>
       <br />
       <hr />
       <br />
       <InfiniteScroll/>
+      <br />
+      <hr />
+      <br />
     </div>
   );
 }
