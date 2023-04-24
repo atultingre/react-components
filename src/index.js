3eec0e48bbdb44e2d2fd00c838931171f076b736
ATUL TINGRE <atultingre.work@gmail.com>
2023-04-25 04:17:36 +0530

    Scrollbar Added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
+
 import { QueryClient, QueryClientProvider } from "react-query";
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
