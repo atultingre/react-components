03cbc4ac287ce17cf9094e425a7a067536ecca5f
ATUL TINGRE <atultingre.work@gmail.com>
2023-04-25 14:44:44 +0530

    new skeleton added and css for that

--- src/Skeleton/SkeletonPost.jsx
+++ src/Skeleton/SkeletonPost.jsx
@@ -4,13 +4,42 @@ import Skeleton from "./Skeleton";
 
 const SkeletonPost = () => {
   return (
-    <div className="post">
-      <Skeleton classes="profile-circle width-50" />
-      <Skeleton classes="title width-50" />
-      <Skeleton classes="text width-60" />
-      <Skeleton classes="text width-80" />
-      <Skeleton classes="text width-100" />
-    </div>
+    <>
+      <div className="post">
+        <Skeleton classes="profile-circle width-50" />
+        <Skeleton classes="title width-50" />
+        <Skeleton classes="text width-60" />
+        <Skeleton classes="text width-80" />
+        <Skeleton classes="text width-100" />
+      </div>
+
+      <div className="post">
+        <Skeleton classes="title width-50" />
+        <Skeleton classes="text width-60" />
+        <Skeleton classes="text width-80" />
+        <Skeleton classes="text width-100" />
+        <div className="skeleton-flex">
+          <Skeleton classes="text width-10 circle" />
+          <Skeleton classes="text width-10 circle" />
+          <Skeleton classes="text width-10 circle" />
+        </div>
+      </div>
+
+      <div className="post">
+        <Skeleton classes="profile-square width-50" />
+        <Skeleton classes="title width-50" />
+        <Skeleton classes="text width-60" />
+        <Skeleton classes="text width-80" />
+        <Skeleton classes="text width-100" />
+      </div>
+
+      <div className="post">
+        <Skeleton classes="title width-50" />
+        <Skeleton classes="text width-60" />
+        <Skeleton classes="text width-80" />
+        <Skeleton classes="text width-100" />
+      </div>
+    </>
   );
 };
 
